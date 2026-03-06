58c8eb201d1d1e99f8d72a859da2c75fb2205993
Aritra <aritra.03@proton.me>
2026-03-06 20:00:10 +0530

    Fixed import name in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { authMiddleware } from "./middlewares/auth.js";
 
-import addserv from "./routes/addService.js";
+import addkey from "./routes/addService.js";
 
 const app = new Hono();
 
