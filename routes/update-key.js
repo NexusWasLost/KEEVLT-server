24a016a985e9f576cfcd898d82892847fd00782c
Aritra <aritra.03@proton.me>
2026-03-13 11:51:49 +0530

    added logic to delete cached key data on key update

--- routes/update-key.js
+++ routes/update-key.js
@@ -5,6 +5,10 @@ import { HTTPException } from "hono/http-exception";
 const updatekey = new Hono();
 
 updatekey.patch("/update-key/:key_id", async function (c) {
+    const meta = c.get("meta");
+    if(!meta)
+        throw new HTTPException(401, { message: "User context not found (Unauthorized) !" });
+
     const keyId = c.req.param("key_id");
     if (!keyId)
         throw new HTTPException(400, { message: "Key Id not provided" });
@@ -20,6 +24,9 @@ updatekey.patch("/update-key/:key_id", async function (c) {
         RETURNING *`,
         [newAPIKeyName, newServiceName, keyId]
     );
+
+    //remove all the cached key data
+    c.executionCtx.waitUntil(c.env.API_CACHE.delete(`keys:${meta.uid}`));
 
     if (data.length === 0) {
         throw new HTTPException(404, { message: "Key not found" });
